0a94e0f232d5a55574b1dd0597aa694abd380479
javierpl99 <59649522+javierpl99@users.noreply.github.com>
2020-01-28 12:04:18 +0100

    Incidencia en móviles

--- Tienda moviles/src/Incidencia.java
+++ Tienda moviles/src/Incidencia.java
@@ -1,4 +1,49 @@
-
 public class Incidencia {
+	private String nAveria, fechaInicio, diagnostico, resolucion, fechaCierre;
+	private Movil movil;
+	
+	public Incidencia(String nAveria, String fechaInicio, Movil movil) {
+		super();
+		this.nAveria = nAveria;
+		this.fechaInicio = fechaInicio;
+		this.movil = movil;
+	}
 
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
+
+	public String getResolucion() {
+		return resolucion;
+	}
+
+	public void setResolucion(String resolucion) {
+		this.resolucion = resolucion;
+	}
+
+	public String getFechaCierre() {
+		return fechaCierre;
+	}
+
+	public void setFechaCierre(String fechaCierre) {
+		this.fechaCierre = fechaCierre;
+	}
+
+	public String getnAveria() {
+		return nAveria;
+	}
+
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public Movil getMonitor() {
+		return movil;
+	}
+	
 }
+
